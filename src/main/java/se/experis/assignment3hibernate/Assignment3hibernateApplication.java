408408e65f76233b0a53ef26f341985634371861
kikkipedia <kristinakerlund@gmail.com>
2021-07-26 13:13:15 +0200

    first commit

---
+++ src/main/java/se/experis/assignment3hibernate/Assignment3hibernateApplication.java
@@ -0,0 +1,13 @@
+package se.experis.assignment3hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3hibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment3hibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/experis/assignment3hibernate/Assignment3hibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.assignment3hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3hibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
